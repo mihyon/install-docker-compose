3fd89e99e45c302ca35b23b98f03b0fd8e30fdf0
mihyon <m-git@hirano.cc>
2021-08-23 02:18:57 -0400

    fix

--- install-docker-compose-latest.sh
+++ install-docker-compose-latest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 output='/usr/local/bin/docker-compose'
 curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-$(uname -s)-$(uname -m) -o $output
